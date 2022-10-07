7645966e6309fe18a0a091c971a8910b8d7f3b87
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2022-10-07 17:59:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProgressBar from "./components/progress_bar/ProgressBar";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [progress, setProgress] = useState(0);
+  const [intervalId, setIntervalId] = useState(0);
+  const state = {
+    size: 250,
+    progress,
+    strokeWidth: 15,
+    circleOneStroke: "#d9edfe",
+    circleTwoStroke: "#7ea9e1",
+  };
+
+  const startCounting = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(0);
+      return;
+    }
+
+    const newIntervalId = setInterval(() => {
+      setProgress((prevCount) => (prevCount === 100 ? 100 : prevCount + 1));
+    }, 100);
+    setIntervalId(newIntervalId);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ProgressBar {...state} />
+        <button className="button" onClick={startCounting}>
+          {intervalId ? "Stop" : "Start"}
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/progress_bar/ProgressBar.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./ProgressBar.css";
+
+const ProgressBar = (props) => {
+  const [offset, setOffset] = useState(0);
+  const { size, progress, strokeWidth, circleOneStroke, circleTwoStroke } =
+    props;
+
+  const center = size / 2;
+  const radius = size / 2 - strokeWidth / 2;
+  const circumference = 2 * Math.PI * radius;
+
+  useEffect(() => {
+    const progressOffset = ((100 - progress) / 100) * circumference;
+    setOffset(progressOffset);
+  }, [setOffset, progress, circumference, offset]);
+
+  return (
+    <div>
+      <svg className="circular_chart" width={size} height={size}>
+        <circle
+          className="circular_bg"
+          stroke={circleOneStroke}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={strokeWidth}
+        ></circle>
+        <circle
+          className="circle"
+          stroke={circleTwoStroke}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={strokeWidth}
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+        ></circle>
+        <text x={center} y={center} className="percentage">
+          {progress}%
+        </text>
+      </svg>
+    </div>
+  );
+};
+
+export default ProgressBar;
